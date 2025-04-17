deeac09c78c98ec61e44cf58d82f517693078b41
Rennn0 <leqterhanibal@gmail.com>
2025-04-17 16:51:45 +0400

    hush

--- __BENCH__/Program.cs
+++ __BENCH__/Program.cs
@@ -1,14 +1,9 @@
-﻿using __BENCH__.Models;
-using __BENCH__.Source;
-
+﻿using __BENCH__.Source;
 // using __BENCH__.Source;
 using BenchmarkDotNet.Running;
-using Dapper;
-using Microsoft.Data.SqlClient;
-using Microsoft.EntityFrameworkCore;
 
 // BenchmarkDotNet.Reports.Summary summary = BenchmarkRunner.Run<DbBench>();
 
 BenchmarkDotNet.Reports.Summary summary = BenchmarkRunner.Run<DbCursorVsWhileBench>();
 
-// [x]  vs resharper source kods amoyris?  amoyara 
+// [x]  vs resharper source kods amoyris?  amoyara
