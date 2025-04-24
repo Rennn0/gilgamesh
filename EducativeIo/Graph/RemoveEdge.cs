a8907b74ee0bd757406fbb723d9350963abf67ee
Rennn0 <leqterhanibal@gmail.com>
2025-04-24 13:01:58 +0400

    simple af

---
+++ EducativeIo/Graph/RemoveEdge.cs
@@ -0,0 +1,10 @@
+﻿namespace EducativeIo.Graph;
+
+public partial class Solution
+{
+    public static void RemoveEdge(Graph g, int source, int destination)
+    {
+        LinkedList adj = g.GetArray()[source];
+        adj.Delete(destination);
+    }
+}
