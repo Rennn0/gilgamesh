dc76325a3900ba0db31b3c20e969aa8ce077cf8c
Rennn0 <leqterhanibal@gmail.com>
2025-04-24 14:06:38 +0400

    shortest poath

---
+++ EducativeIo/Graph/ShortestPath.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+
+namespace EducativeIo.Graph;
+
+public partial class Solution
+{
+    public static string ShortestPath(Graph g, int source, int destination)
+    {
+        if (source == destination)
+            return string.Empty;
+
+        Dictionary<int, int> parentChild = new Dictionary<int, int>();
+        Queue<int> currentLayer = new Queue<int>();
+        Queue<int> nextLayer = new Queue<int>();
+
+        currentLayer.Enqueue(source);
+        parentChild[source] = -1;
+
+        while (currentLayer.Count > 0)
+        {
+            int node = currentLayer.Dequeue();
+            if (node == destination)
+                return ReconstructPath(parentChild, source, destination);
+
+            LinkedList.Node adj = g.GetArray()[node].GetHead();
+            while (adj != null)
+            {
+                if (parentChild.TryAdd(adj.m_data, node))
+                {
+                    nextLayer.Enqueue(adj.m_data);
+                }
+
+                adj = adj.m_nextElement;
+            }
+
+            if (currentLayer.Count != 0)
+                continue;
+
+            currentLayer = nextLayer;
+            nextLayer = new Queue<int>();
+        }
+
+        return string.Empty;
+    }
+
+    private static string ReconstructPath(Dictionary<int, int> dic, int source, int destination)
+    {
+        StringBuilder pathBuilder = new StringBuilder();
+
+        string current = destination.ToString();
+        while (dic.TryGetValue(int.Parse(current), out int parent))
+        {
+            pathBuilder.Insert(0, $"{current} ");
+            current = parent.ToString();
+        }
+
+        pathBuilder.Length--;
+        return pathBuilder.ToString();
+    }
+}
--- __TESTS__/DirectedGraphTests.cs
+++ __TESTS__/DirectedGraphTests.cs
@@ -226,4 +226,53 @@ public class DirectedGraphTests
         g.AddEdge(5, 6);
         Assert.AreEqual(4, Solution.FindMinWithArr(g, 1, 6));
     }
+
+    [TestMethod]
+    public void Graph_ShortestPath()
+    {
+        Graph g = new Graph(6);
+        g.AddEdge(0, 1);
+        g.AddEdge(0, 2);
+        g.AddEdge(0, 3);
+        g.AddEdge(2, 4);
+        g.AddEdge(3, 5);
+        g.AddEdge(5, 4);
+
+        Assert.AreEqual("0 2 4", Solution.ShortestPath(g, 0, 4));
+    }
+
+    [TestMethod]
+    public void Graph_ShortestPath1()
+    {
+        Graph g = new Graph(7);
+        g.AddEdge(0, 1);
+        g.AddEdge(0, 2);
+        g.AddEdge(0, 3);
+        g.AddEdge(1, 2);
+        g.AddEdge(2, 4);
+        g.AddEdge(2, 3);
+        g.AddEdge(3, 5);
+        g.AddEdge(5, 4);
+        g.AddEdge(5, 6);
+        Assert.AreEqual("1 2 3 5 6", Solution.ShortestPath(g, 1, 6));
+    }
+
+    [TestMethod]
+    public void Graph_ShortestPath2()
+    {
+        Graph g = new Graph(8);
+        g.AddEdge(0, 1);
+        g.AddEdge(0, 2);
+        g.AddEdge(0, 3);
+        g.AddEdge(1, 4);
+        // g.AddEdge(2, 5);
+        g.AddEdge(2, 6);
+        g.AddEdge(3, 6);
+        g.AddEdge(2, 2);
+        g.AddEdge(5, 4);
+        g.AddEdge(5, 7);
+        g.AddEdge(6, 5);
+
+        Assert.AreEqual("0 3 6 5 7", Solution.ShortestPath(g, 0, 7));
+    }
 }
