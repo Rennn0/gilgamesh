6df3dae76a074c891c0925d61c3d46889520b7da
Rennn0 <leqterhanibal@gmail.com>
2025-05-06 00:44:37 +0400

    lil stuff

--- Apps/Client/main.js
+++ Apps/Client/main.js
@@ -1,8 +1,15 @@
 import { EventSource } from "eventsource"
 
-const sse = new EventSource("http://localhost:5076/api/sse/subscribe/luka")
-sse.onopen = $e => console.log("Connection opened", $e)
-sse.onmessage = $e => console.log(JSON.stringify($e.data))
-sse.onerror = $e => console.error($e)
+try {
+    const sse = new EventSource("https://gilgamesh.publicvm.com/api/sse/subscribe/luka")
 
-sse.addEventListener("heartbeat", $e => console.log("Heartbeat", JSON.stringify($e.data)));
+    sse.onopen = $e => console.log("Connection opened", $e)
+    sse.onmessage = $e => console.log("Message received", JSON.stringify($e.data))
+    sse.onclose = $e => console.log("Connection closed", $e)
+    sse.onerror = $e => console.error($e)
+
+    sse.addEventListener("heartbeat", $e => console.log("Heartbeat", JSON.stringify($e.data)));
+
+} catch (error) {
+    console.error("Error initializing EventSource:", error)
+}
