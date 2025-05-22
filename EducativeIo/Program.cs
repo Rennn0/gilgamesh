0cece64882b1a937bd349b85e0104c7626dc9bd8
Rennn0 <leqterhanibal@gmail.com>
2025-05-22 22:11:28 +0400

    concurency

--- EducativeIo/Program.cs
+++ EducativeIo/Program.cs
@@ -21,8 +21,23 @@ internal class Solution
     private static SemaphoreSlim semaCons = new SemaphoreSlim(3);
 
     static int dummy = 0;
+
+    static EventWaitHandle eventWaitHandleManual = new EventWaitHandle(false, EventResetMode.ManualReset);
+    static EventWaitHandle eventWaitHandleAuto = new EventWaitHandle(false, EventResetMode.AutoReset);
+
+    static AutoResetEvent are1 = new AutoResetEvent(false);
+    static AutoResetEvent are2 = new AutoResetEvent(false);
+
+    static ManualResetEvent mre1 = new ManualResetEvent(false);
+    static ManualResetEvent mre2 = new ManualResetEvent(false);
+
+    static bool printTurn = false;
+    static volatile bool shutDown = false;
+    static int val = 0;
+    static object pad = new object();
     public static void Main(string[] args)
     {
+
         // new Thread(Ping) { IsBackground = true }.Start();
         // new Thread(Pong) { IsBackground = true }.Start();
 
@@ -67,29 +82,160 @@ internal class Solution
         //     Console.WriteLine("Semaphore released");
         // }
 
-        Thread[] threads = new Thread[10];
-        for (int i = 0; i < threads.Length; i++)
-        {
-            threads[i] = new Thread(() =>
-            {
-                for (int j = 0; j < 10000; j++)
-                {
-                    // dummy++;
-                    Interlocked.Add(ref dummy, 1);
-                }
-            });
-        }
-        for (int i = 0; i < threads.Length; i++)
-        {
-            threads[i].Start();
-        }
-        for (int i = 0; i < threads.Length; i++)
-        {
-            threads[i].Join();
-        }
+        // Thread[] threads = new Thread[10];
+        // for (int i = 0; i < threads.Length; i++)
+        // {
+        //     threads[i] = new Thread(() =>
+        //     {
+        //         for (int j = 0; j < 10000; j++)
+        //         {
+        //             // dummy++;
+        //             Interlocked.Add(ref dummy, 1);
+        //         }
+        //     });
+        // }
+        // for (int i = 0; i < threads.Length; i++)
+        // {
+        //     threads[i].Start();
+        // }
+        // for (int i = 0; i < threads.Length; i++)
+        // {
+        //     threads[i].Join();
+        // }
 
-        Console.WriteLine(dummy);
+        // Console.WriteLine(dummy);
+
+        // ThreadPool.QueueUserWorkItem(_ =>
+        // {
+        //     // eventWaitHandleManual.WaitOne();
+        //     // are1.WaitOne();
+        //     mre1.WaitOne();
+        //     Console.WriteLine("Manual event signaled 1");
+        //     mre2.Set();
+        //     // are2.Set();
+        //     // eventWaitHandleAuto.Set();
+        // });
+
+        // ThreadPool.QueueUserWorkItem(_ =>
+        // {
+        //     // eventWaitHandleManual.WaitOne();
+        //     // are1.WaitOne();
+        //     mre1.WaitOne();
+        //     Console.WriteLine("Manual event signaled 2");
+        //     mre2.Set();
+        //     // are2.Set();
+        //     // eventWaitHandleAuto.Set();
+        // });
+
+        // // WaitHandle.SignalAndWait(eventWaitHandleManual, eventWaitHandleAuto);
+        // // are1.Set();
+        // // are2.WaitOne();
+
+        // mre1.Set();
+        // mre2.WaitOne();
+
+        // // mre1.Reset();
+        // mre1.WaitOne();
+        // Console.WriteLine("Main thread exiting");
+
+        // Thread tp1 = new Thread(Printer) { IsBackground = true };
+        // Thread tp2 = new Thread(Printer) { IsBackground = true };
+        // Thread tp3 = new Thread(Printer) { IsBackground = true };
+        // Thread tf1 = new Thread(Finder) { IsBackground = true };
+        // Thread tf2 = new Thread(Finder) { IsBackground = true };
+
+        // tp1.Start();
+        // tp2.Start();
+        // tp3.Start();
+        // tf1.Start();
+        // tf2.Start();
+
+        // Thread.Sleep(15000);
+        // shutDown = true;
+        // Console.WriteLine("Shutting down");
+
+        // tp1.Join();
+        // tp2.Join();
+        // tp3.Join();
+        // tf1.Join();
+        // tf2.Join();
+
+        // bool flag = false;
+        // object obj = new object();
+
+        // ThreadPool.QueueUserWorkItem(_ =>
+        // {
+        //     while (true)
+        //     {
+        //         Monitor.Enter(obj);
+
+        //         while (flag) Monitor.Wait(obj);
+
+        //         Console.WriteLine("Ping");
+        //         flag = !flag;
+        //         Thread.Sleep(1000);
+        //         Monitor.Exit(obj);
+        //     }
+        // });
+        // ThreadPool.QueueUserWorkItem(_ =>
+        // {
+        //     while (true)
+        //     {
+        //         Monitor.Enter(obj);
+
+        //         while (!flag) Monitor.Wait(obj);
+
+        //         Console.WriteLine("Pong");
+        //         flag = !flag;
+        //         Thread.Sleep(2000);
+        //         Monitor.Exit(obj);
+        //     }
+        // });
+        // ThreadPool.QueueUserWorkItem(state =>
+        // {
+        //     while (true)
+        //     {
+        //         ThreadPool.GetAvailableThreads(out int workerThreads, out int completionPortion);
+        //         ThreadPool.GetMaxThreads(out int maxWorker, out int maxCompl);
+        //         Console.WriteLine($"{workerThreads}  {completionPortion}  {maxWorker} {maxCompl}");
+        //         Thread.Sleep(1000);
+        //     }
+        // }, new object(), true);
+        // Console.ReadKey();
+
+        new QuizQuestion().run();
     }
+
+    public static void Printer()
+    {
+        while (!shutDown)
+        {
+            Monitor.Enter(pad);
+            while (!printTurn && !shutDown) Monitor.Wait(pad);
+
+            printTurn = false;
+            Console.WriteLine($"Thread {System.Environment.CurrentManagedThreadId} printing {val}");
+            Thread.Sleep(1000);
+            Monitor.PulseAll(pad);
+            Monitor.Exit(pad);
+        }
+    }
+    public static void Finder()
+    {
+        while (!shutDown)
+        {
+            Monitor.Enter(pad);
+            while (printTurn && !shutDown) Monitor.Wait(pad);
+
+            printTurn = true;
+            val = new Random().Next(1, 100);
+            Console.WriteLine($"Thread {System.Environment.CurrentManagedThreadId} finding {val}");
+            Thread.Sleep(2000);
+            Monitor.PulseAll(pad);
+            Monitor.Exit(pad);
+        }
+    }
+
     public static async Task Producer(int id)
     {
         while (true)
@@ -209,4 +355,70 @@ internal class Solution
         }
     }
 
+}
+
+// public class QuizQuestion
+// {
+
+//     private readonly Object obj = new Object();
+
+
+//     void MonitorExit()
+//     {
+//         Thread.Sleep(500);
+//         Monitor.Exit(obj);
+//     }
+
+
+//     void MonitorEnter()
+//     {
+//         Monitor.Enter(obj);
+//         Thread.Sleep(500);
+//     }
+
+//     public void run()
+//     {
+
+//         Thread t1 = new Thread(new ThreadStart(MonitorEnter));
+//         Thread t2 = new Thread(new ThreadStart(MonitorExit));
+
+//         t1.Start();
+//         t2.Start();
+
+//         t1.Join();
+//         t2.Join();
+
+//         Console.WriteLine("Hello");
+//         Monitor.Enter(obj);
+//         Console.WriteLine("World");
+//         Monitor.Exit(obj);
+//     }
+// }
+public class QuizQuestion
+{
+    private Object obj = false;
+
+    public void printMessage()
+    {
+
+        Monitor.Enter(obj);
+        obj = true;
+        Thread.Sleep(3000);
+        Monitor.Exit(obj);
+        Console.WriteLine("All is good");
+    }
+
+    public void run()
+    {
+
+        Thread t1 = new Thread(new ThreadStart(printMessage));
+        t1.Start();
+
+        Thread t2 = new Thread(new ThreadStart(printMessage));
+        Thread.Sleep(5000);
+        t2.Start();
+
+        t1.Join();
+        t2.Join();
+    }
 }
