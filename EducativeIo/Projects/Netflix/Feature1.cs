e6993662bb5e7cf9a61eaa5779d0366f80f29603
Rennn0 <leqterhanibal@gmail.com>
2025-05-29 12:42:17 +0400

    anagaram

---
+++ EducativeIo/Projects/Netflix/Feature1.cs
@@ -0,0 +1,69 @@
+﻿using System.Text;
+
+namespace EducativeIo.Projects.Netflix;
+
+/// <summary>
+///     Group Similar Titles
+/// </summary>
+public partial class Netflix
+{
+    private readonly Dictionary<string, LinkedList<string>> _anagrams;
+
+    private const int AlphabetSize = 26;
+
+    public Netflix()
+    {
+        _anagrams = new Dictionary<string, LinkedList<string>>();
+    }
+
+    /// <summary>
+    ///     Create vector for anagram, example: "abbcc" => "#1#2#3#0#0...."
+    /// </summary>
+    /// <param name="input"></param>
+    /// <returns></returns>
+    protected virtual string CreateVector(string input)
+    {
+        StringBuilder builder = new StringBuilder();
+
+        int[] vector = new int[AlphabetSize];
+        foreach (
+            int index in input.Select(GetIndex).Where(index => index is >= 0 and < AlphabetSize)
+        )
+        {
+            vector[index]++;
+        }
+
+        for (int i = 0; i < AlphabetSize; i++)
+        {
+            builder.Append($"#{vector[i]}");
+        }
+
+        return builder.ToString();
+    }
+
+    protected virtual int GetIndex(char c) => c - 'a';
+
+    public virtual void AddRange(IEnumerable<string> input) => input.ToList().ForEach(Add);
+
+    public virtual void Add(string input)
+    {
+        string vector = CreateVector(input);
+
+        if (_anagrams.TryGetValue(vector, out var anagram))
+        {
+            anagram.AddFirst(input);
+        }
+        else
+        {
+            _anagrams[vector] = new LinkedList<string>([input]);
+        }
+    }
+
+    public virtual IEnumerable<string> Search(string input)
+    {
+        string vector = CreateVector(input);
+        if (!_anagrams.TryGetValue(vector, out LinkedList<string>? anagram))
+            return [];
+        return anagram.OrderBy(title => title.Equals(input, StringComparison.Ordinal) ? 0 : 1);
+    }
+}
---
+++ __TESTS__/Netflix/GroupSimilarTitles.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.ObjectModel;
+
+namespace __TESTS__.Netflix;
+
+[TestClass]
+public class GroupSimilarTitles
+{
+    [TestMethod]
+    public void Test()
+    {
+        EducativeIo.Projects.Netflix.Netflix netflix = new EducativeIo.Projects.Netflix.Netflix();
+
+        netflix.AddRange(["abbcc", "dule", "duel", "deul", "speed", "spede", "cars"]);
+        ReadOnlyCollection<string> result = netflix.Search("duel").ToArray().AsReadOnly();
+
+        Assert.AreEqual("duel", result.ElementAt(0));
+        Assert.AreEqual(3, result.Count);
+    }
+}
