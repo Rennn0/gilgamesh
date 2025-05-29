018bb3b888b5db02535803d0cfd27d935c2a4854
Rennn0 <leqterhanibal@gmail.com>
2025-05-29 22:10:05 +0400

    merge

--- EducativeIo/Projects/Netflix/MedianAge.cs
+++ EducativeIo/Projects/Netflix/MedianAge.cs
@@ -1,3 +1,4 @@
+using EducativeIo.BoundedBuffer;
 using EducativeIo.Heap;
 
 namespace EducativeIo.Projects.Netflix
@@ -6,7 +7,7 @@ namespace EducativeIo.Projects.Netflix
     {
         private readonly MaxHeap<int> _max = new MaxHeap<int>();
         private readonly MinHeap<int> _min = new MinHeap<int>();
-
+        private readonly List<int> _justList = new List<int>();
         public void InsertAge(int age)
         {
             if (_max.Size() == 1 || _max.GetMax() >= age)
@@ -28,10 +29,25 @@ namespace EducativeIo.Projects.Netflix
                 _max.Insert(_min.GetMin());
                 _min.RemoveMin();
             }
+
+            // _justList.Add(age);
         }
 
         public float MedianAge()
         {
+            // MultiThreadedMergeSort mtms = new MultiThreadedMergeSort();
+            // int[] arr = _justList.ToArray();
+            // mtms.Sort(0, _justList.Count - 1, arr);
+
+            // if (arr.Length % 2 == 0)
+            // {
+            //     return (arr[arr.Length / 2] + arr[arr.Length / 2 - 1]) / 2f;
+            // }
+            // else
+            // {
+            //     return arr[arr.Length / 2];
+            // }
+
             if (_max.Size() == _min.Size())
             {
                 return (_max.GetMax() + _min.GetMin()) / 2f;
