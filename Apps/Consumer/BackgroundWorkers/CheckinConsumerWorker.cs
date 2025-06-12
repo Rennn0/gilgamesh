182c2be24778c0366d5f40d7a27a68c5f37e216b
Rennn0 <leqterhanibal@gmail.com>
2025-06-12 23:51:11 +0400

    rbt

--- Apps/Consumer/BackgroundWorkers/CheckinConsumerWorker.cs
+++ Apps/Consumer/BackgroundWorkers/CheckinConsumerWorker.cs
@@ -1,7 +1,7 @@
 ﻿using System.Text;
 using Consumer.Settings;
 using Core.Rabbit;
-using Microsoft.Extensions.Configuration;
+using Core.Rabbit.Options;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
@@ -13,6 +13,7 @@ namespace Consumer.BackgroundWorkers
     {
         private readonly ILogger<CheckinConsumerWorker> _logger;
         private readonly RabbitBasicDirectConsumer _consumer;
+        private readonly RabbitBasicDirectConsumer _consumer2;
         private const int MessageReceivedEid = 2001;
         private const int StartedListeningEid = 1000;
 
@@ -32,23 +33,42 @@ namespace Consumer.BackgroundWorkers
                 mqSettings.Port
             );
 
+            _consumer2 = new RabbitBasicDirectConsumer(
+                nameof(CheckinConsumerWorker),
+                mqSettings.Queue,
+                mqSettings.Host,
+                mqSettings.Username,
+                mqSettings.Password,
+                mqSettings.Port
+            );
+
             _logger = logger;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            await _consumer.StartListeningAsync();
+            await _consumer.StartListeningAsync(new ChannelCreationOptions(prefetchcount: 0));
+            await _consumer2.StartListeningAsync(new ChannelCreationOptions(prefetchcount: 0));
 
             _logger.LogDebug(new EventId(StartedListeningEid), "StartedListening");
 
             _consumer.AttachCallback(CallbackAsync);
+            _consumer2.AttachCallback(CallbackAsync2);
         }
-
         private async Task CallbackAsync(object sender, BasicDeliverEventArgs @event)
         {
             _logger.LogDebug(
                 new EventId(MessageReceivedEid),
-                $"RECEIVED MESSAGE {Encoding.UTF8.GetString(@event.Body.ToArray())}"
+                $"1"
+            );
+            await _consumer.AcknowledgeAsync(@event.DeliveryTag);
+        }
+
+        private async Task CallbackAsync2(object sender, BasicDeliverEventArgs @event)
+        {
+            _logger.LogDebug(
+                new EventId(MessageReceivedEid),
+                $"2"
             );
             await _consumer.AcknowledgeAsync(@event.DeliveryTag);
         }
