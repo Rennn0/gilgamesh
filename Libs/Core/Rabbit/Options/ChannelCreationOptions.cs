60a3acd84bfb245a2258c7e01683048ee9931f98
Rennn0 <leqterhanibal@gmail.com>
2025-06-12 23:13:27 +0400

    options

--- Libs/Core/Rabbit/Options/ChannelCreationOptions.cs
+++ Libs/Core/Rabbit/Options/ChannelCreationOptions.cs
@@ -2,6 +2,23 @@ namespace Core.Rabbit.Options
 {
     public struct ChannelCreationOptions
     {
+        public ChannelCreationOptions(
+            uint prefetchsize = 0,
+            ushort prefetchcount = 5,
+            bool global = false,
+            bool durable = false,
+            bool exclusive = false,
+            bool autoDelete = false,
+            bool autoAck = false)
+        {
+            Prefetchsize = prefetchsize;
+            Prefetchcount = prefetchcount;
+            Global = global;
+            Durable = durable;
+            Exclusive = exclusive;
+            AutoDelete = autoDelete;
+            AutoAck = autoAck;
+        }
         public readonly uint Prefetchsize;
         public readonly ushort Prefetchcount;
         public readonly bool Global;
