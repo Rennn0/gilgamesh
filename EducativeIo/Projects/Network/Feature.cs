d80da2cf91272e97ab4ce3281474d723e9be5679
Rennn0 <leqterhanibal@gmail.com>
2025-08-18 23:04:25 +0400

    nw

--- EducativeIo/Projects/Network/Feature.cs
+++ EducativeIo/Projects/Network/Feature.cs
@@ -1,3 +1,6 @@
+using System.Reflection.Metadata;
+using EducativeIo.Hash;
+
 namespace EducativeIo.Projects.Network
 {
     public class Feature
@@ -48,6 +51,86 @@ namespace EducativeIo.Projects.Network
             }
 
             return result;
+        }
+
+        public static List<int> GetDevices(NetworkNode root, NetworkNode server, int ttl)
+        {
+            Dictionary<int, List<int>> neighbors = new Dictionary<int, List<int>>();
+            Dfs(null, root, neighbors);
+
+            List<int> bfs = new List<int>() { server.Data };
+            HashSet<int> lookup = new HashSet<int>(bfs);
+
+            for (int i = 0; i < ttl; i++)
+            {
+                List<int> temp = new List<int>();
+                foreach (int node in bfs)
+                {
+                    foreach (int n in neighbors[node])
+                    {
+                        if (lookup.Contains(n))
+                        {
+                            continue;
+                        }
+                        temp.Add(n);
+                    }
+                }
+
+                bfs = temp;
+                lookup.UnionWith(bfs);
+            }
+
+            return bfs;
+        }
+
+        private static void Dfs(
+            NetworkNode? parent,
+            NetworkNode? child,
+            Dictionary<int, List<int>> neighbors
+        )
+        {
+            if (child is null)
+            {
+                return;
+            }
+
+            if (parent is not null)
+            {
+                if (neighbors.TryGetValue(parent.Data, out List<int>? pVal))
+                {
+                    pVal.Add(child.Data);
+                }
+                else
+                {
+                    neighbors[parent.Data] = new List<int> { child.Data };
+                }
+
+                if (neighbors.TryGetValue(child.Data, out List<int>? cVal))
+                {
+                    cVal.Add(parent.Data);
+                }
+                else
+                {
+                    neighbors[child.Data] = new List<int> { parent.Data };
+                }
+            }
+
+            for (int i = 0; i < child.Children.Count; i++)
+            {
+                Dfs(child, child.Children[i], neighbors);
+            }
+        }
+
+        public class NetworkNode
+        {
+            public int Data { get; }
+            public List<NetworkNode> Children { get; }
+
+            public NetworkNode(int data)
+            {
+                Data = data;
+                Children = new List<NetworkNode>();
+            }
         }
     }
 }
